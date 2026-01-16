6b0f85372ae8dae5b40fd2059c77f69d0069fdf6
Developer Greenlab <dev.greenlab@gmail.com>
2026-01-16 01:03:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7786152721-d1bea",
+  "appId": "1:640433587211:web:abfa85276f50b759c5f30e",
+  "apiKey": "AIzaSyDFScgZ_qL2U7IGISlqLnI6fGwe06cgm4E",
+  "authDomain": "studio-7786152721-d1bea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "640433587211"
+};
