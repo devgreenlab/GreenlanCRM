7fb27eae6f666b3995d68411110107def1f3a614
Developer Greenlab <dev.greenlab@gmail.com>
2026-01-21 06:15:34 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/obrolan/[[...leadId]]/page.tsx
+++
@@ -1,6 +0,0 @@
-// This file is intentionally left blank to resolve a routing conflict.
-// Its logic has been split into /obrolan/page.tsx and /obrolan/[leadId]/page.tsx
-// Exporting null ensures this file does not generate a route.
-export default function ObrolanCatchAllPage() {
-    return null;
-}
